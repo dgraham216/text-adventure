122d0c59c95f7aa102155cf11a4b4f394b7d89bc
dgraham216 <dgraham216@gmail.com>
2023-07-30 21:44:09 -0700

    It works!!!!
    Next to add Time Stamps??

--- second_text_adventure.py
+++ second_text_adventure.py
@@ -18,9 +18,9 @@ def intro():
     print("It is bolted in place")
     print()
     print("Next you investigate the backpack. You pick it up and unbuckle the flap.")
-    print("Inside you find a screwdriver, a match, a wrapped piece of your favorite chewing gum, and a hammer.")
+    print("Inside you find a screwdriver, a match, a wrapped piece of your favorite minty bubble gum, and a hammer.")
     print("Your stomach growls.  You might as well pop in that piece of chewing gum. This might take awhile.")
-    print()
+    print('You notice the logo on the gum wrapper as you happily chomp away.  It reads "Fresh Breath".')
     intro2()
 
 
@@ -48,6 +48,9 @@ def intro2():
     elif firstDecision == '5':
         print()
         fire()
+
+
+
 
 def north():
     print('You walk north. This wall has a closed elevator door. An elevator button next to the door has an engraved arrow pointing down.') 
@@ -81,22 +84,82 @@ def east():
         print("Really? Sometimes the obvious choice is the right choice. Let's try this again.")
         east()
          
-
-
-
-
 def south():
-    print()
+    print('You walk south to barn style double doors. They are red with white trim. With hope in your heart, you lift the latch, place one hand on each doorpull and swing them towards you.') 
+    print('Pulling them open wide, you find a shimmery slimy cobweb covering the doorway.')
+    print('The moment your hand reaches out to sweep the web aside, two giant metalic pincers from the giant, sparking spider now framing the doorway jolt your body rigid with electricity.') 
+    print('This must be the end...')
+    print('OR is it?')
+    print('As you shake uncontrollably, and your eyes roll to the back of your head, your left foot slips on a puddle of slime from the web.')
+    print('Miraculously, your leg swings up as your body falls down, and you kick the spider back with your rubber toed boots.')
+    print('With the pincers no longer zapping you, your head clears, and you have just enough strength to get to your knees, reach out, and slam both doors shut.')
+    print('Latch slammed in place!') 
+    print("It takes you a few minutes to recover...that was close!")
+    intro2()
 
 
 def west():
     print()
+    print('You find yourself facing a large black glass window. You can not see out the window.')
+    remove = input('Would you like to use the hammer in your bag to break the window? (y/n)\n>> ')
+    if remove == 'y':
+        print('The black glass shatters spraying glass everywhere! The window is completely gone. Thick green clouds of poisonous gas begin to fill the room and your lungs [Ending 3/4 Found].')
+        print('OR does it?')
+        print('As you strain with pain to hold your breath, you remember the bubble gum you have been chewing!')
+        print('You have a hunch...')
+        print('You somehow blow a surprisingly large lavender bubble with what little air you have left.')
+        print('It grows to the size of your face and then pops over your nose and mouth.') 
+        print('Your hunch was correct, the bubblegum forms an oxygen releasing mask over your nose and mouth, protecting you from the gas in the room.')
+        intro2()
+    elif remove == 'n':
+        print("Really? You're here. You have a hammer. It should easily break this glass.  Haven't you ever wanted to smash glass?")
+        print('Smash the glass!  Smash the glass!  Smash the glass!')
+        print("You just can't resist.")   
+        print('The black glass shatters spraying glass everywhere! The window is completely gone. Thick green clouds of poisonous gas begin to fill the room and your lungs [Ending 3/4 Found].')
+        print('OR does it?')
+        print('As you strain with pain to hold your breath, you remember the bubble gum you have been chewing!')
+        print("You have a hunch...")
+        print('You somehow blow a surprisingly large lavender bubble with what little air you have left.')
+        print('It grows to the size of your face and then pops over your nose and mouth.') 
+        print("Your hunch was correct...after all, the gum was called 'Fresh Breath'. The bubblegum forms an oxygen releasing mask over your nose and mouth, thus protecting you from the gas in the room.")
+        print('After a minute to catch your breath...the green gas disipates. You head back to the center of the room.')
+        intro2()
+
 
 
 def fire():
-    print()
+    print('You search around the room once more. It seems like an impossible answer, but why else would the match have been in the bag?')
+    print('You pull the match out of the bag and open the door to the woodburning stove.')
+    print('You strike the match against the iron door and the match sparks to life.')
+    print('You light the kindling and within seconds the dry wood piled above it begins to catch.')
+    print('Can your body handle any more heat?  Will this be the end? Was there something else you could have tried?')
+    print('As your eyes search around the room for any other option, the wood is now fully ablaze.')
+    print('The black pipe leading up to the ceiling begins to shake and shimmy, rumble and creek!')
+    print('In fact the whole ceiling seems to be rippling, waving, almost fluttering in all the commotion!')
+    print('In fact, it seems to be expanding upwards!')
+    print('You can not believe your eyes! Your body is slammed to the floor as the entire room seems to lifting up!')
+    print('The realization becomes solid in your mind.')
+    print('You are in a giant hot air balloon soaring up through the cleaer blue sky!')
+    print('You Are Free!!!')
+    ending()
 
+def ending():
+    print('The wood in the stove quickly burns down to coals, and the balloon gently descends.')
+    print('As you sit in a corner, still stunned from your experience, you hear a "ding" from the elevator door.')
+    print('The elevator doors slide open...')
+    print('You stand up, walk to the door, and step outside')
+    print('onto')
+    print('the MOON!?!')
+    closing()
 
+def closing():
+    print("          ##############################")
+    print("          #                            #")
+    print("          #   Thank You For Playing!   #")
+    print("          #                            #")
+    print("          ##############################")   
+
+    
 
 
     
@@ -119,7 +182,7 @@ print()
 print()
 print("          ##############################")
 print("          #                            #")
-print("          #      Title Here            #")
+print("          #       Going Somewhere?     #")
 print("          #                            #")
 print("          ##############################")
 print()
